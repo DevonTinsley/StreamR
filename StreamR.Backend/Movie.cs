02dfdceaff3d0eb28cf692d31987a6b11e86270b
Devon Tinsley <dwt2307@gmail.com>
2019-11-04 18:10:46 -0600

    Added Movie repository

---
+++ StreamR.Backend/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StreamR.Backend
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Categories { get; set; }
+        public string Synopsis { get; set; }
+        public int LengthInMinutes { get; set; }
+
+
+    }
+}
---
+++ StreamR.Tests/StreamerBackend.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StreamR.Tests
+{
+    public class StreamerBackend
+    {
+        [Fact]
+        public void MovieGeneratorWorking()
+        {
+
+        }
+    }
+}
