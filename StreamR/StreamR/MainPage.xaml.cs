12c82d2b27d1ac1f338c3919fc3445007300532a
Brandon Kimball <Brandon.Kimball82@gmail.com>
2019-11-25 14:56:23 -0600

    Added profile page platform selection and other minor adjustments to the overall app

--- StreamR/StreamR/MainPage.xaml.cs
+++ StreamR/StreamR/MainPage.xaml.cs
@@ -19,51 +19,12 @@ namespace StreamR
 
             GenrePicker.ItemsSource = new List<string> { "Action", "Adventure", "Comedy" };
 
-            bool tvIsToggled = TVSwitch.IsToggled;
-            bool movieIsToggled = MovieSwitch.IsToggled;
 
-            
-            
-            
-            while (tvIsToggled == true)
-            {
-                if (movieIsToggled == true)
-                    tvIsToggled = false;
-            }
-
-            while (movieIsToggled == true)
-            {
-                if (tvIsToggled == true)
-                    movieIsToggled = false;
-            }
-        }
-
-        private void TVSwitch_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if(TVSwitch.IsToggled == true)
-            {
-                if (MovieSwitch.IsToggled)
-                {
-                    MovieSwitch.IsToggled = false;
-                    TVSwitch.IsToggled = true;
-                }
-            }
-        }
-
-        private void MovieSwitch_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (MovieSwitch.IsToggled == true)
-            {
-                if (TVSwitch.IsToggled)
-                {
-                    TVSwitch.IsToggled = false;
-                }
-            }
         }
 
         private void Search_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new MovieListPage());
+            Navigation.PushAsync(new SearchPage());
         }
 
         private void Monitor_Clicked(object sender, EventArgs e)
@@ -74,6 +35,11 @@ namespace StreamR
         private void Profile_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new ProfilePage());
+        }
+
+        private void Search_For_Movie(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MovieListPage());
         }
     }
 }
--- StreamR/StreamR/ProfilePage.xaml.cs
+++ StreamR/StreamR/ProfilePage.xaml.cs
@@ -33,12 +33,80 @@ namespace StreamR
         private void HuluSwitch_Toggled(object sender, ToggledEventArgs e)
         {
             if (HuluSwitch.IsToggled == true) {
-
-                platforms.Add("Hulu");
-                
+                platforms.Add("Hulu");               
             }
             if (HuluSwitch.IsToggled == false) {
                 platforms.Remove("Hulu");
+            }
+        }
+
+        private void NetflixSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (NetflixSwitch.IsToggled == true) {
+                platforms.Add("Netflix");
+            }
+            if (NetflixSwitch.IsToggled == false) {
+                platforms.Remove("Netflix");
+            }
+        }
+
+        private void AmazonPrimeSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (AmazonPrimeSwitch.IsToggled == true) { 
+                platforms.Add("AmazonPrime");
+            }
+            if (AmazonPrimeSwitch.IsToggled == false) {
+                platforms.Remove("AmazonPrime");
+            }
+        }
+
+        private void HBOSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (HBOSwitch.IsToggled == true) {
+                platforms.Add("HBO");
+            }
+            if (HBOSwitch.IsToggled == false) {
+                platforms.Remove("HBO");
+            }
+        }
+
+        private void ShowtimeSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (ShowtimeSwitch.IsToggled == true) {
+                platforms.Add("Showtime");
+            }
+            if (ShowtimeSwitch.IsToggled == false) {
+                platforms.Remove("Showtime");
+            }
+        }
+
+        private void StarsSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (StarsSwitch.IsToggled == true) {
+                platforms.Add("Stars");
+            }
+            if (StarsSwitch.IsToggled == false) {
+                platforms.Remove("Stars");
+            }
+        }
+
+        private void SlingSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (SlingSwitch.IsToggled == true) {
+                platforms.Add("Sling");
+            }
+            if (SlingSwitch.IsToggled == false) {
+                platforms.Remove("Sling");
+            }
+        }
+
+        private void DisneySwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (DisneySwitch.IsToggled == true) {
+                platforms.Add("Disney");
+            }
+            if (DisneySwitch.IsToggled == false) {
+                platforms.Remove("Disney");
             }
         }
     }
